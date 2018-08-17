a9cfc323acbc4b2e5df05330c9a9002696b0d4f8
Christopher <chrispritchard.nz@gmail.com>
2018-08-18 08:34:32 +1200

    Fixed up colours

--- View.fs
+++ View.fs
@@ -12,11 +12,13 @@ let assetsToLoad = [
     Texture ("white", "./Content/white.png")
 ]
 
-let (tx, ty) = 40, 40
+let (tx, ty) = 20, 20
+let wallColour = new Color(50,50,50)
+let openColour = Color.White
 
 let getView runState worldState =
     match worldState with
     | MapView map ->
         let blocks = map |> List.map (fun (Tile (x, y, wall)) -> 
-            Image ("white", (x*tx,y*ty,tx,ty), if wall then Color.Gray else Color.White))
+            Image ("white", (x*tx,y*ty,tx,ty), if wall then wallColour else openColour))
         blocks
