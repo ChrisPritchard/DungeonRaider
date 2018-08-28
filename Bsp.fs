22f0fb0d82773811427fc043484c8c72ebb16407
Christopher <chrispritchard.nz@gmail.com>
2018-08-28 20:40:48 +1200

    moved adjacency out of dungeon method needs a rethink

--- Bsp.fs
+++ Bsp.fs
@@ -104,6 +104,21 @@ let rec joined bspResult =
             yield! spaces2
     } |> Seq.toList
 
+let getOpenAdjacency x y tiles =
+    let adjacent = 
+        tiles 
+        |> List.filter 
+            (fun (Tile (ox, oy, kind)) -> 
+                match kind with
+                | Wall _ -> false
+                | _ -> abs (ox - x) <= 1 && abs (oy - y) <= 1 && not (ox = x && oy = y))
+        |> List.map (fun (Tile (ox, oy, _)) -> ox - x, oy - y)
+    adjacent 
+        |> List.map (fun o -> adjacencyKey.Item o) 
+        |> List.fold (fun result k -> result ||| k) 0 
+        |> byte 
+        |> Wall
+
 let dungeon maxSize minLeafSize minRoomSize = 
     let rooms = bspRooms minLeafSize minRoomSize (Range (None, 0, 0, maxSize, maxSize))
     let allOpen = joined rooms
@@ -120,25 +135,10 @@ let dungeon maxSize minLeafSize minRoomSize =
             let inRange = List.tryFind (inRange (x, y)) allOpen
             let kind = match inRange with | Some (Range ((Some kind), _, _, _, _)) -> kind | _ -> Wall 0uy
             Tile (x, y, kind)))
-    
-    let getOpenAdjacency x y =
-        let adjacent = 
-            tiles 
-            |> List.filter 
-                (fun (Tile (ox, oy, kind)) -> 
-                    match kind with
-                    | Wall _ -> false
-                    | _ -> abs (ox - x) <= 1 && abs (oy - y) <= 1 && not (ox = x && oy = y))
-            |> List.map (fun (Tile (ox, oy, _)) -> ox - x, oy - y)
-        adjacent 
-            |> List.map (fun o -> adjacencyKey.Item o) 
-            |> List.fold (fun result k -> result ||| k) 0 
-            |> byte 
-            |> Wall
 
     tiles |> List.map (fun (Tile (x, y, kind)) -> 
         let kind = 
             match kind with 
-            | Wall _ -> getOpenAdjacency x y
+            | Wall _ -> getOpenAdjacency x y tiles
             | _ -> kind
         Tile (x, y, kind))
