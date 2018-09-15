148d254db5a9749550b2f41e5c305a1209e4b370
Christopher <chrispritchard.nz@gmail.com>
2018-09-16 11:43:20 +1200

    Removed commented out code from old model

--- Constants.fs
+++ Constants.fs
@@ -3,6 +3,8 @@ module Constants
 open Microsoft.Xna.Framework.Input
 
 let screenWidth, screenHeight = 800, 800
+let quitKey = Keys.Escape
+
 let midx, midy = screenWidth / 2, screenHeight / 2
 
 let dungeonSize, leafSize, roomSize, minCorridorLength = 30, 7, 5, 3
@@ -13,19 +15,3 @@ let monsterwidth, monsterheight = playerwidth * 3/2, playerheight * 3/2
 
 let frameSpeed = 75.
 let timeBetweenTiles = 250.
-
-// let walkSpeed = tilewidth / 16 |> float
-// // player jiggle
-// let boundaryx, boundaryyup, boundaryydown = float (playerwidth/4), float (playerheight/8), float (playerheight/16)
-
-let quitKey = Keys.Escape
-
-// let leftKeys = [Keys.Left;Keys.A]
-// let rightKeys = [Keys.Right;Keys.D]
-// let upKeys = [Keys.Up;Keys.W]
-// let downKeys = [Keys.Down;Keys.S]
-
-let deathKey = Keys.X
-let gestureKey = Keys.C
-let strikeKey = Keys.F
-let resetKey = Keys.R
--- Controller.fs
+++ Controller.fs
@@ -9,99 +9,6 @@ let getTile x y map =
     if x < 0 || y < 0 || x >= dungeonSize || y >= dungeonSize then None
     else Some <| List.item (x * dungeonSize + y) map
 
-// let nextState runState state =
-//     let elapsed = runState.elapsed
-//     let animFinished start = elapsed - start >= 10. * frameSpeed
-//     match state with
-//     | _ when wasJustPressed resetKey runState ->
-//         Standing elapsed
-//     | Dead -> 
-//         Dead
-//     | Dying start when animFinished start ->
-//         Dead
-//     | _ when wasJustPressed deathKey runState -> 
-//         Dying elapsed
-//     | Standing _ when wasJustPressed gestureKey runState -> 
-//         Gesturing elapsed
-//     | Gesturing start when animFinished start ->
-//         Standing elapsed
-//     | Standing _ when isPressed strikeKey runState -> 
-//         Striking elapsed
-//     // | Walking _ when isPressed strikeKey runState -> 
-//     //     Striking elapsed
-//     | Striking start when animFinished start ->
-//         Standing elapsed
-//     // | Standing _ when isAnyPressed walkKeys runState -> 
-//     //     Walking elapsed
-//     // | Standing _ when isMousePressed (true, false) runState -> 
-//     //     Walking elapsed
-//     // | Walking _ when (isAnyPressed walkKeys runState || isMousePressed (true, false) runState) |> not -> 
-//     //     Standing elapsed
-//     | other -> other
-
-// let nextFacing runState characterState facing = 
-//     let mx, _ = runState.mouse.position
-//     let mouseMove = isMousePressed (true, false) runState
-//     match characterState with
-//     | Standing _ | Walking _ ->
-//         if isAnyPressed leftKeys runState || (mouseMove && mx < midx) then Left
-//         else if isAnyPressed rightKeys runState || (mouseMove && mx > midx) then Right
-//         else facing
-//     | _ -> facing
-
-// let isBlocked (playerx, playery) (Tile (x, y, kind, _)) =
-//     let checkBlock () =
-//         let blockx, blocky = x * tilewidth |> float, y * tileheight |> float
-//         let isOnX =
-//             (playerx - boundaryx >= blockx && playerx - boundaryx < blockx + float tilewidth) // block is left
-//             || (playerx + boundaryx < blockx + float tilewidth && playerx + boundaryx >= blockx) // block is right
-//         let isOnY =
-//             (playery - boundaryyup >= blocky && playery - boundaryyup < blocky + float tileheight) // block is above
-//             || (playery + boundaryydown < blocky + float tileheight && playery + boundaryydown >= blocky) // block is below
-//         isOnX && isOnY
-//     match kind with
-//     | Block _ | StairsUp | StairsDown 1 -> 
-//         checkBlock ()
-//     | _ ->
-//         false
-
-// let getMouseDir (mx, my) (x, y) =
-//     let mx, my = mx - midx, my - midy
-//     let angle = abs (float mx) / abs (float my) |> atan
-//     let dx, dy = sin angle * walkSpeed, cos angle * walkSpeed
-//     x + dx * (if mx < 0 then -1. else 1.), y + dy * (if my < 0 then -1. else 1.)
-
-// let getKeysDir runState = 
-//     [
-//         leftKeys, -1, 0
-//         rightKeys, 1, 0
-//         upKeys, 0, -1
-//         downKeys, 0, 1
-//     ] |> List.fold (fun (rx, ry) (keys, dx, dy) -> 
-//         if isAnyPressed keys runState then 
-//             (rx + dx, ry + dy) 
-//         else 
-//             (rx, ry)) 
-//         (0, 0)
-
-// let nextPosition runState characterState (x, y) tiles =
-//     let (nx, ny) = 
-//         match characterState with
-//         | Walking _ ->
-//             if isMousePressed (true, false) runState then 
-//                 getMouseDir runState.mouse.position (x, y)
-//             else
-//                 getKeysDir runState (x, y)
-//         | _ -> (x, y)    
-    
-//     let (px, py) = (int x / tilewidth, int y / tileheight)
-//     let adjacentTiles = [-1..1] |> List.collect (fun ox -> [-1..1] |> List.map (fun oy -> getTile (ox + px) (oy + py) tiles))
-
-//     let (bx, by) = adjacentTiles |> List.fold (fun (rx, ry) tile -> 
-//         let (bx, by) = isBlocked (nx, y) tile, isBlocked (x, ny) tile
-//         rx || bx, ry || by) (false, false)
-//     (if bx then x else nx), (if by then y else ny)
-
 let mouseTile x y runState = 
     let mx, my = runState.mouse.position
     let relx, rely = midx - mx, midy - my
@@ -158,20 +65,6 @@ let updateEntityPosition runState pathFinder entity =
         | _ -> entity
     | _ -> entity
 
-// let updatePlayerPath map runState player =
-//     let (x, y) = player.position
-//     match getKeysDir runState with
-//     | (0, 0) -> 
-//         player
-//     | (dx, dy) ->
-//         let dest = x + dx, y + dy
-//         match player.path with
-//         | existing::_ when existing = dest -> player
-//         | _ when player.moveStart <> 0. -> player
-//         | _ when not <| isOpen dest map -> player
-//         | _ -> 
-//             { player with path = [(x + dx, y + dy)]; moveStart = runState.elapsed }
-
 let updateEntityFacing entity =
     let (x, _) = entity.position
     match entity.path with
@@ -196,11 +89,6 @@ let advancePlayer map runState =
     updateEntityPosition runState (getNewPlayerPath map)
     >> updateEntityFacing 
     >> updateEntityState
-
-//     let newState = nextState runState player.state
-//     let newFacing = nextFacing runState newState player.facing
-// //    let newPosition = nextPosition runState newState player.position map
-//     { player with state = newState; facing = newFacing }
 
 let advanceMonster map runState monster = 
     monster
