18f4469c726561a65b45185d4f93126e13654ff9
Christopher <chrispritchard.nz@gmail.com>
2018-10-28 16:34:34 +1300

    Upgraded to v 0.0.4 of gamecore

--- Program.fs
+++ Program.fs
@@ -1,14 +1,17 @@
-﻿
-open GameCore.GameLoop
+﻿open GameCore.GameModel
+open GameCore.GameRunner
+open Microsoft.Xna.Framework
 
 [<EntryPoint>]
 let main _ =
-    use game = 
-        new GameLoop<Model.GameModel>(
-            View.resolution, 
-            View.assetsToLoad, 
-            Controller.advanceGame, 
-            View.getView, 
-            Constants.showFPS)
-    game.Run ()
+
+    let config = {
+        clearColour = Some Color.Black
+        assetsToLoad = View.assetsToLoad
+        resolution = View.resolution
+        fpsFont = None   
+    }
+
+    runGame config Controller.advanceGame View.getView
+
     0
